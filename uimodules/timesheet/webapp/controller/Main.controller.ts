72b9dcb2119ec506147d3af6f5b7d86a0178d459
Mads Rosengreen Jørgensen <mrj@gavdilabs.com>
2024-05-13 19:32:59 +0200

    Played around with planning calendar properties

--- uimodules/timesheet/webapp/controller/Main.controller.ts
+++ uimodules/timesheet/webapp/controller/Main.controller.ts
@@ -1,11 +1,26 @@
-import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import Event from "sap/ui/base/Event";
+import ResponsivePopover from "sap/m/ResponsivePopover";
+import Fragment from "sap/ui/core/Fragment";
+import Button from "sap/m/Button";
 
 /**
  * @namespace trix.timesheet.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+	private timeRegPopover: ResponsivePopover;
+
+
+	async onNewTimeReg(oEvent: Event) {
+		const oView = this.getView();
+		if (!this.timeRegPopover) {
+			this.timeRegPopover = (await Fragment.load({
+				id: oView.getId(),
+				name: "trix.timesheet.view.fragments.TimeRegPopover",
+				controller: this,
+			})) as ResponsivePopover;
+			oView.addDependent(this.timeRegPopover);
+		}
+		this.timeRegPopover.openBy(oView.byId("page"))
 	}
 }
