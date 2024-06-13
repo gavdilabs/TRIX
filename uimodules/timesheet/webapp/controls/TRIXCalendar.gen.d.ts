a6576fdbdc1c10fd2fc826bdea82ea1a134cdc0a
acconsultant <ach@gavdilabs.com>
2024-06-13 21:21:39 +0200

    DefaultView now part of custom control

--- uimodules/timesheet/webapp/controls/TRIXCalendar.gen.d.ts
+++ uimodules/timesheet/webapp/controls/TRIXCalendar.gen.d.ts
@@ -1,4 +1,3 @@
-import CalendarWeekNumbering from "sap/ui/core/date/CalendarWeekNumbering";
 import { PropertyBindingInfo } from "sap/ui/base/ManagedObject";
 import { $SinglePlanningCalendarSettings } from "sap/m/SinglePlanningCalendar";
 
@@ -8,96 +7,28 @@ declare module "./TRIXCalendar" {
      * Interface defining the settings object used in constructor calls
      */
     interface $TRIXCalendarSettings extends $SinglePlanningCalendarSettings {
-        calendarWeekNumbering?: CalendarWeekNumbering | PropertyBindingInfo | `{${string}}`;
-        enableAppointmentsCreate?: boolean | PropertyBindingInfo | `{${string}}`;
-        enableAppointmentsDragAndDrop?: boolean | PropertyBindingInfo | `{${string}}`;
-        enableAppointmentsResize?: boolean | PropertyBindingInfo | `{${string}}`;
+        defaultView?: string | PropertyBindingInfo;
     }
 
     export default interface TRIXCalendar {
 
-        // property: calendarWeekNumbering
+        // property: defaultView
 
         /**
-         * Gets current value of property "calendarWeekNumbering".
+         * Gets current value of property "defaultView".
          *
-         * Default value is: "CalendarWeekNumbering.Default,"
-         * @returns Value of property "calendarWeekNumbering"
+         * @returns Value of property "defaultView"
          */
-        getCalendarWeekNumbering(): CalendarWeekNumbering;
+        getDefaultView(): string;
 
         /**
-         * Sets a new value for property "calendarWeekNumbering".
+         * Sets a new value for property "defaultView".
          *
          * When called with a value of "null" or "undefined", the default value of the property will be restored.
          *
-         * Default value is: "CalendarWeekNumbering.Default,"
-         * @param [calendarWeekNumbering="CalendarWeekNumbering.Default,"] New value for property "calendarWeekNumbering"
+         * @param defaultView New value for property "defaultView"
          * @returns Reference to "this" in order to allow method chaining
          */
-        setCalendarWeekNumbering(calendarWeekNumbering: CalendarWeekNumbering): this;
-
-        // property: enableAppointmentsCreate
-
-        /**
-         * Gets current value of property "enableAppointmentsCreate".
-         *
-         * Default value is: true
-         * @returns Value of property "enableAppointmentsCreate"
-         */
-        getEnableAppointmentsCreate(): boolean;
-
-        /**
-         * Sets a new value for property "enableAppointmentsCreate".
-         *
-         * When called with a value of "null" or "undefined", the default value of the property will be restored.
-         *
-         * Default value is: true
-         * @param [enableAppointmentsCreate=true] New value for property "enableAppointmentsCreate"
-         * @returns Reference to "this" in order to allow method chaining
-         */
-        setEnableAppointmentsCreate(enableAppointmentsCreate: boolean): this;
-
-        // property: enableAppointmentsDragAndDrop
-
-        /**
-         * Gets current value of property "enableAppointmentsDragAndDrop".
-         *
-         * Default value is: true
-         * @returns Value of property "enableAppointmentsDragAndDrop"
-         */
-        getEnableAppointmentsDragAndDrop(): boolean;
-
-        /**
-         * Sets a new value for property "enableAppointmentsDragAndDrop".
-         *
-         * When called with a value of "null" or "undefined", the default value of the property will be restored.
-         *
-         * Default value is: true
-         * @param [enableAppointmentsDragAndDrop=true] New value for property "enableAppointmentsDragAndDrop"
-         * @returns Reference to "this" in order to allow method chaining
-         */
-        setEnableAppointmentsDragAndDrop(enableAppointmentsDragAndDrop: boolean): this;
-
-        // property: enableAppointmentsResize
-
-        /**
-         * Gets current value of property "enableAppointmentsResize".
-         *
-         * Default value is: true
-         * @returns Value of property "enableAppointmentsResize"
-         */
-        getEnableAppointmentsResize(): boolean;
-
-        /**
-         * Sets a new value for property "enableAppointmentsResize".
-         *
-         * When called with a value of "null" or "undefined", the default value of the property will be restored.
-         *
-         * Default value is: true
-         * @param [enableAppointmentsResize=true] New value for property "enableAppointmentsResize"
-         * @returns Reference to "this" in order to allow method chaining
-         */
-        setEnableAppointmentsResize(enableAppointmentsResize: boolean): this;
+        setDefaultView(defaultView: string): this;
     }
 }
--- uimodules/timesheet/webapp/controls/TRIXCalendar.ts
+++ uimodules/timesheet/webapp/controls/TRIXCalendar.ts
@@ -4,9 +4,25 @@ import RenderManager from "sap/ui/core/RenderManager";
  * @namespace trix.timesheet.controls
  */
 export default class TRIXCalendar extends SinglePlanningCalendar {
+	private defaultViewSet: boolean = false;
+
 	static readonly metadata = {
-		properties: {},
+		properties: {
+			defaultView: {
+				type: "string",
+			},
+		},
 	};
+
+	onAfterRendering(oEvent: jQuery.Event): void {
+		super.onAfterRendering(oEvent);
+
+		//Handle default view setting (custom)
+		if (this.getDefaultView() && !this.defaultViewSet) {
+			this.setSelectedView(this.getViewByKey(this.getDefaultView()));
+			this.defaultViewSet = true;
+		}
+	}
 
 	static renderer = RenderManager.getRenderer(SinglePlanningCalendar.prototype);
 }
